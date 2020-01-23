4820ed4ee18a5a10b96ad560b79ec9c8e4d6a49c
rh2740 <rh2740@nyu.edu>
2020-01-23 17:12:18 -0500

    1.23.11

--- Scripts/prep.py
+++ Scripts/prep.py
@@ -235,14 +235,14 @@ def input_handler():
                 print("Invalid Image! Try again!")
                 imagefile = None
         # enter hyperparameters
-        if batchsize is None: batchsize = int(input("Please input batch size (DEFAULT=24; ENTER to skip): " or 24))
-        if epoch is None: epoch = int(input("Please input epoch size (DEFAULT=infinity; ENTER to skip): " or 100000))
+        if batchsize is None: batchsize = int(input("Please input batch size (DEFAULT=24; ENTER to skip): ")) or 24
+        if epoch is None: epoch = int(input("Please input epoch size (DEFAULT=infinity; ENTER to skip): ")) or 100000
         if resolution is None:
-            resolution = int(input("Please input the max resolution of slides (ENTER to skip): ") or None)
+            resolution = int(input("Please input the max resolution of slides (ENTER to skip): ")) or None
         # enter BMI and age if known
         if mode == "test":
-            if not isinstance(BMI, float): BMI = float(input("Please input patient BMI (ENTER to skip): ") or np.nan)
-            if not isinstance(age, float): age = float(input("Please input patient age (ENTER to skip): ") or np.nan)
+            if not isinstance(BMI, float): BMI = float(input("Please input patient BMI (ENTER to skip): ")) or np.nan
+            if not isinstance(age, float): age = float(input("Please input patient age (ENTER to skip): ")) or np.nan
         # enter label file
         if label_file is None and mode != "test":
             label_file = str(input("Please input full path to label file (ENTER to skip): ")) or None
